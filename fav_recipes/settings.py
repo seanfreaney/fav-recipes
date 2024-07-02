b1950a42dfdc8613cc81704aa4be5866b5a645fd
Sean Freaney <seanfreaney123@gmail.com>
2024-07-02 17:41:27 +0000

    add database

--- fav_recipes/settings.py
+++ fav_recipes/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bw+@wrm+3bzf+floz&@8d5zgu2ka!b8y#0mh5xq@zwi*yro_1m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["8000-seanfreaney-favrecipes-cm5ufqhc64p.ws.codeinstitute-ide.net",
                 ".herokuapp.com"]
@@ -75,11 +79,15 @@ WSGI_APPLICATION = 'fav_recipes.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
