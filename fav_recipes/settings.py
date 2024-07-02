f04b992022bd2605279526bdfbc91724d9ec63c7
Sean Freaney <seanfreaney123@gmail.com>
2024-07-02 18:02:41 +0000

    add secret key

--- fav_recipes/settings.py
+++ fav_recipes/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bw+@wrm+3bzf+floz&@8d5zgu2ka!b8y#0mh5xq@zwi*yro_1m'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["8000-seanfreaney-favrecipes-cm5ufqhc64p.ws.codeinstitute-ide.net",
                 ".herokuapp.com"]
