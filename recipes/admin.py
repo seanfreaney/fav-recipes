80ec72c23c49315e93c6400e2a27cb4a3ce11e26
Sean Freaney <seanfreaney123@gmail.com>
2024-07-04 20:23:15 +0000

    register Category, Ingredient, RecipeIngredient and RecipeCategory models in admin.py

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Recipe
+from .models import Category, Ingredient, Recipe, RecipeIngredient, RecipeCategory
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Ingredient)
 admin.site.register(Recipe)
+admin.site.register(RecipeIngredient)
+admin.site.register(RecipeCategory)
