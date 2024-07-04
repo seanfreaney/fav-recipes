d8df34f6741cc54ff89c8483d8b40af6c217606d
Sean Freaney <seanfreaney123@gmail.com>
2024-07-04 19:49:35 +0000

    add Category, Ingredient, RecipeIngredient and RecipeCategory models

--- recipes/models.py
+++ recipes/models.py
@@ -10,7 +10,22 @@ class Recipe(models.Model):
     description = models.TextField()
     instructions = models.TextField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    # categories = models.ManyToManyField(Category, through='RecipeCategory')
-    # ingredients = models.ManyToManyField(Ingredient, through='RecipeIngredient')
+    categories = models.ManyToManyField(Category, through='RecipeCategory')
+    ingredients = models.ManyToManyField(Ingredient, through='RecipeIngredient')
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    quantity = models.CharField(max_length=100)
+
+class RecipeCategory(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
