474b9b63c6ce2c83181db3dfb24ba628cd7aaa23
Sean Freaney <seanfreaney123@gmail.com>
2024-07-07 07:31:19 +0000

    change migrations, add method to recipe model, input ingredients.json data

--- recipes/models.py
+++ recipes/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 STATUS = ((0, "Draft"), (1, "Published")) #logic from codestar blog
 
+
 # Create your models here.
+
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
@@ -11,8 +14,13 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
 
 class Recipe(models.Model):
     title = models.CharField(max_length=200)
@@ -24,10 +32,15 @@ class Recipe(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
 
+    def __str__(self):
+        return self.title
+
+
 class RecipeIngredient(models.Model):
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
     ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
     quantity = models.CharField(max_length=100)
+
 
 class RecipeCategory(models.Model):
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
