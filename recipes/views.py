02835a87a337fc6dbd34d4a546fea9f56ef82fb2
Sean Freaney <seanfreaney123@gmail.com>
2024-07-13 12:08:48 +0000

    add authentication for create recipe

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views import generic
 from .models import Recipe
