12fcf088563378f4a1b5e31b73e8e0cf4ee7dd36
Sean Freaney <seanfreaney123@gmail.com>
2024-07-20 10:55:27 +0000

    test recipe detail

--- recipes/test_views.py
+++ recipes/test_views.py
@@ -1,30 +1,47 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.urls import reverse
 from django.contrib.auth.models import User
-from recipes.models import Recipe, Ingredient, Category
+from .models import Recipe, Category, Ingredient, RecipeIngredient, RecipeCategory
 
-
-class RecipeListViewTest(TestCase):
-
+class RecipeDetailViewTests(TestCase):
     def setUp(self):
-        self.user = User.objects.create_user(username='testuser', password='testpassword')
-        
-    def test_recipe_list_view_with_filters(self):
-        Recipe.objects.create(title='Recipe 1', description='Description', instructions='Instructions', status=1, user=self.user)
-        Recipe.objects.create(title='Recipe 2', description='Description', instructions='Instructions', status=0, user=self.user)
-        
-        response = self.client.get(reverse('home'))
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'recipes/recipe_list.html')
-        self.assertContains(response, 'Recipe 1')
-        self.assertNotContains(response, 'Recipe 2')
+        # Create a user
+        self.user = User.objects.create_user(username='testuser', password='12345')
 
-    def test_pagination(self):
-        for i in range(30):
-            Recipe.objects.create(title=f'Recipe {i}', description='Description', instructions='Instructions', status=1, user=self.user)
+        # Create category
+        self.category1 = Category.objects.create(pk=1)
 
-        response = self.client.get(reverse('home'))
+        # Create ingredients
+        self.ingredient1 = Ingredient.objects.create(pk=2)
+        self.ingredient2 = Ingredient.objects.create(pk=3)
+
+        # Create a published recipe
+        self.recipe = Recipe.objects.create(
+            title='Test Recipe',
+            description='This is a test recipe',
+            instructions='Mix ingredients and bake.',
+            user=self.user,
+            status=1
+        )
+
+        # Associate categories and ingredients with the recipe
+        RecipeCategory.objects.create(recipe=self.recipe, category=self.category1)
+        RecipeIngredient.objects.create(recipe=self.recipe, ingredient=self.ingredient1, quantity='1 cup')
+        RecipeIngredient.objects.create(recipe=self.recipe, ingredient=self.ingredient2, quantity='2 cups')
+
+        self.client = Client()
+
+    def test_recipe_detail_view_with_published_recipe(self):
+        response = self.client.get(reverse('recipe_detail', args=[self.recipe.pk]))
         self.assertEqual(response.status_code, 200)
-        self.assertTrue('is_paginated' in response.context)
-        self.assertTrue(response.context['is_paginated'] is True)
-        self.assertEqual(len(response.context['recipe_list']), 10)
+        self.assertTemplateUsed(response, 'recipes/recipe_detail.html')
+        self.assertContains(response, self.recipe.title)
+        self.assertContains(response, self.recipe.description)
+        self.assertContains(response, self.ingredient1.name)
+        self.assertContains(response, self.ingredient2.name)
+
+    # to be run once draft posting available to user
+    # def test_recipe_detail_view_with_draft_recipe(self):
+    #    response = self.client.get(reverse('recipe_detail', args=[self.draft_recipe.pk]))
+    #    self.assertEqual(response.status_code, 404)
+    
